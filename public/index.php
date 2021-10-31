5921e25c4f1f2e33fe867bb76a27d4c97bb86793
Gabriel De Los Rios <gab.delosrios@gmail.com>
2021-10-31 19:48:05 -0300

    test

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 */
 
 require __DIR__.'/../vendor/autoload.php';
-Header("location: /index.html");
+Header("Location: /index.html");
 /*
 |--------------------------------------------------------------------------
 | Run The Application
