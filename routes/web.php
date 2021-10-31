1e25f6460b6e92e9ebeee71b7053f0d021e01b29
Gabriel De Los Rios <gab.delosrios@gmail.com>
2021-10-31 19:56:58 -0300

    This would be horrendous but lets see if this works

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Header("Location: /index.html");
+    return Header("Location: https://gabtestlaravelapp.herokuapp.com/index.html");
 });
